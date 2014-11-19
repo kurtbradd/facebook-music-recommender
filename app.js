e8e012b35a2411cc67c036b50c825d4beffbe159
Dylan Aspden <dhaspden@gmail.com>
2014-11-19 18:55:05 -0500

    Cleaned up some of the code.

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 
-var http = require('http');
-
 var expressConfig = require('./config/expressConfig');
 var errorHandlers = require('./config/errorHandlers');
 
+var http = require('http');
 var app = express();
 expressConfig.setup(app, express);
 errorHandlers.setup(app);
 
 module.exports = app;
-
-// DEBUG=facebook-music-recommender ./bin/www 
-// DEBUG=facebook-music-recommender nodemon ./bin/www 

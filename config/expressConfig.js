484ab1b905c139834eaaa9c464c03930f1d9655f
Dylan Aspden <dhaspden@gmail.com>
2015-01-05 18:36:49 -0500

    Add express-session and create a new session

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -7,11 +7,13 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 exports.setup = function(app, express) {
 	app.use(logger('dev'));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(cookieParser());
+  app.use(session({ secret: 'mylittlesecret' , resave: false, saveUninitialized: false }));
 	app.use(express.static(path.join(__dirname, 'public')));
 }

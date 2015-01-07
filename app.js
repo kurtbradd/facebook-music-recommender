d0d2a959ec279cd7cdbc84fae46f140e9f342752
Dylan Aspden <dhaspden@gmail.com>
2015-01-06 19:41:11 -0500

    Fix bugs for serving the Angular application

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var passport = require('passport');
 var path = require('path');
 var favicon = require('serve-favicon');
 
-var expressConfig = require('./config/expressConfig');
-var passportConfig = require('./config/passportConfig');
+var expressConfig = require('./config/express-config');
+var passportConfig = require('./config/passport-config');
 
 var app = express();
 
--- config/express-config.js
+++ config/express-config.js
@@ -10,10 +10,12 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 exports.setup = function(app, express) {
-	app.use(logger('dev'));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(cookieParser());
+  app.set('views', 'public/views');
+  app.engine('html', require('ejs').renderFile);
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use(session({ secret: 'mylittlesecret' , resave: false, saveUninitialized: false }));
-	app.use(express.static(path.join(__dirname, 'public')));
+  app.use(cookieParser());
+  app.use(express.static('./public'));
 }

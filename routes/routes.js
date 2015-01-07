36258bda3c042ea34f4688c04f00534674deb3a1
Dylan Aspden <dhaspden@gmail.com>
2015-01-06 19:42:28 -0500

    Add catchall route for serving Angular application

--- routes/routes.js
+++ routes/routes.js
@@ -20,4 +20,8 @@ module.exports = function(app, passport) {
 
   app.get('/api/genre', genreController.getGenres);
   app.get('/api/genre/artists', genreController.getArtistsByGenre);
+
+  app.get('*', function(req, res) {
+    return res.render('index.html');
+  });
 };

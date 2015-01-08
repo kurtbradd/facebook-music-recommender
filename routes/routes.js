4e35bbcc85351ff97aeacd3674d16612c94e78be
Dylan Aspden <dhaspden@gmail.com>
2015-01-07 20:10:40 -0500

    Change route file to be used later

--- routes/routes.js
+++ routes/routes.js
@@ -6,10 +6,9 @@ module.exports = function(app, passport) {
 
   app.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
-    failureRedirect : '/'
-  }), function(req, res) {
-    return res.status(200).send(req.user);
-  });
+    failureRedirect : '/',
+    successRedirect : '/'
+  }));
 
   app.get('/profile', function(req, res) {
     return res.send(200);

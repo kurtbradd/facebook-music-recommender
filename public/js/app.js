0042b7ef5a5a1b8d675fe4631c3451867c39d0f0
Dylan Aspden <dhaspden@gmail.com>
2015-01-07 20:11:05 -0500

    Setup the UI-Router and state for main page

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,22 @@
 var dependencies = ['ui.router', 'app.controllers', 'app.directives', 'app.services'];
 var app = angular.module('app', dependencies);
+
+app.run(['$rootScope', '$state', '$stateParams',
+  function($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+  } 
+]);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
+  function($stateProvider, $urlRouteProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $urlRouteProvider.otherwise('/');
+    $stateProvider
+    .state('home', {
+      url: '/',
+      controller: 'LoginController',
+      templateUrl: 'views/landing-page.html'
+    });
+  }
+]);

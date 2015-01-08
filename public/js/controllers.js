4d187ee682faa321839e3e3120240ab5ba7209c6
Dylan Aspden <dhaspden@gmail.com>
2015-01-07 20:11:52 -0500

    Setup the basic login service to log into the app

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,1 +1,10 @@
 var module = angular.module('app.controllers', []);
+
+module.controller('LoginController', ['$scope', '$state', '$window', 'LoginService',
+  function($scope, $state, $window, LoginService) {
+    console.log('LoginController has loaded');
+    $scope.login = function() {
+      LoginService.login();
+    }
+  }
+]);
--- public/js/services.js
+++ public/js/services.js
@@ -1,1 +1,11 @@
 var module = angular.module('app.services', []);
+
+module.factory('LoginService', ['$window', 
+  function($window) {
+    return { 
+      login: function() {
+        $window.location.href = '/auth/facebook';
+      }
+    }
+  }
+])

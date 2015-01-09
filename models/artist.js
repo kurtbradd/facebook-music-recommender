8985335d2f4909491953c95b6e5f967b854e1f3d
Dylan Aspden <dhaspden@gmail.com>
2015-01-08 21:13:37 -0500

    Change facebookId in model to BIGINT

--- models/artist.js
+++ models/artist.js
@@ -3,7 +3,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Artist = sequelize.define('Artist', {
     id:         { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    facebookId: { type: DataTypes.INTEGER, unique: true, allowNull: false }
+    facebookId: { type: DataTypes.BIGINT, unique: true, allowNull: false }
   }, {
     timestamps: false,
     tableName: 'Artists'

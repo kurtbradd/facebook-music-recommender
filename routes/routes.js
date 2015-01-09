373f43985bf52c42568d84802e7cdb981f89c9c5
Dylan Aspden <dhaspden@gmail.com>
2015-01-09 18:44:52 -0500

    Change user permissions

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ var genreController = require('../controllers/genre-controller');
 var queue = require('../workers/queue');
 
 module.exports = function(app, passport) {
-  app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email', 'user_likes'] }));
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['user_likes', 'user_location'] }));
 
   app.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
@@ -19,10 +19,10 @@ module.exports = function(app, passport) {
     return res.send(200);
   });
 
-  app.post('/api/artist/:id/like', artistController.likeArtist);
+  app.post('/api/artist/:id/like',      artistController.likeArtist);
   app.get('/api/artist/recommendation', artistController.getArtistRecommendations);
 
-  app.get('/api/genre', genreController.getGenres);
+  app.get('/api/genre',         genreController.getGenres);
   app.get('/api/genre/artists', genreController.getArtistsByGenre);
 
   app.get('*', function(req, res) {

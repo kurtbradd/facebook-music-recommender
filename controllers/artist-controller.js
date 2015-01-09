3e9b6607373ff6516b5b743842fd7ca573145af4
Dylan Aspden <dhaspden@gmail.com>
2015-01-08 21:51:18 -0500

    Implement the API call to like artists in database

--- controllers/artist-controller.js
+++ controllers/artist-controller.js
@@ -2,7 +2,27 @@ var model = require('../models/index');
 
 /* Sets a like in the Like table for a given artist and user (by Facebook ID number) */
 exports.likeArtist = function(req, res) {
-  return res.status(200).send('Success');
+  if (!req.user) return res.status(400).send('Not logged in');
+
+  var facebookId = req.params.id;
+  model.Artist.find({ where: { facebookId: facebookId } })
+  .then(function(result) {
+    if (result === null) {
+      return res.status(400).send('No artist found');
+    }
+    else {
+      model.Like.findOrCreate({ where: { user: req.user.id, artist: result.values.id } })
+      .spread(function(user, created) {
+        return res.status(200).send(user.values);
+      })
+      .catch(function(err) {
+        return res.status(400).send(err);
+      });
+    }
+  })
+  .catch(function(err) {
+    return res.status(400).send(err);
+  });
 }
 
 /* Retrieves the artist recommendations for the user */

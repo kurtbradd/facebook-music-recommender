3d98bd9e6a61fd6ad472f6149e19df2e9447ee57
Dylan Aspden <dhaspden@gmail.com>
2015-01-11 00:58:03 -0500

    Add cookie functionality to the server

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,6 +3,6 @@ var _ = require('lodash');
 exports.isLoggedIn = function(req, res) {
   if (_.isEmpty(req.user)) { return res.status(400).send({ error: 'Not logged in' }); }
   else {
-    return res.status(200).send({ user: req.user });
+    return res.status(200).send({ user: req.user, session: req.session });
   }
 }

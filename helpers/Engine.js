9839c6550220d537e1b14d2e7916c7f51dae5f34
Kurt DaCosta <kurtbradd@gmail.com>
2015-01-12 01:03:48 -0500

    uses predictionio-driver package

--- helpers/Engine.js
+++ helpers/Engine.js
@@ -1,7 +1,23 @@
-var prediction = require('predictionio')({
-    key: 'O25CzUErhnRXR6tEL5KgF4CfMo73WmrXNFm9ykYfK5Dlpg1d5mtqLMySfe2m2cFI',
-    baseUrl: 'http://162.243.227.24:8000/'
-  })
-	prediction.items.get(14, function (err, res) {
-    console.log(err, res)
-  })
+var predictionio = require('predictionio-driver');
+var config = require('../config/keys.js').predictionio;
+
+var engineClient = new predictionio.Engine({ url: config.engineUrl });
+var eventClient = new predictionio.Events({
+	appId: config.appId, 
+	accessKey: config.appKey, 
+	url: config.eventUrl
+});
+
+
+eventClient.status()
+.then(function(status) {
+	console.log(status);
+});
+
+engineClient.sendQuery({
+	user: 3,
+	num: 3
+})
+.then(function (result) {
+	console.log(result);
+});

bb9aed7ab2282d23ac606267b1319f392dffdbcb
Dylan Aspden <dhaspden@gmail.com>
2015-01-12 15:29:00 -0500

    Refactor routes and implement new calls

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 var artistController = require('../controllers/artist-controller');
+var likeController = require('../controllers/like-controller');
 var genreController = require('../controllers/genre-controller');
 var authController = require('../controllers/auth-controller');
 var queue = require('../workers/queue');
@@ -8,33 +9,25 @@ module.exports = function(app, passport) {
 
   app.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
-    failureRedirect : '/'
-  }),
-  function(req, res) {
-    console.log('made it');
-    queue.crawlUser(req.user.values);
-    console.log(req.session.cookie);
-    res.clearCookie('user')
-    .redirect('/main')
-    .send(req.session.cookie);
-  });
+    failureRedirect : '/' }), 
+    authController.login
+  );
 
-  app.get('/profile', function(req, res) {
-    return res.send(200);
-  });
+  app.get('/api/session',                authController.isLoggedIn);
+  app.get('/api/logout',                 authController.logout);
 
-  app.get('/api/session', authController.isLoggedIn);
+  app.post('/api/like/:id',              likeController.setLike);
+  app.get('/api/like',                   likeController.getLikes);
 
-  app.post('/api/artist/:id/like',      artistController.likeArtist);
-  app.get('/api/artist/:id',            artistController.getArtist);
-  app.get('/api/artist/:id/genre',      artistController.getGenreOfArtist);
+  app.get('/api/artist/:id',             artistController.getArtist);
+  app.get('/api/artist/:id/genre',       artistController.getGenreOfArtist);
 
-  app.get('/api/genre',         genreController.getGenres);
-  app.get('/api/genre/artists', genreController.getArtistsByGenre);
+  app.get('/api/genre',                  genreController.getGenres);
+  app.get('/api/genre/artists',          genreController.getArtistsByGenre);
 
   app.get('/api/artist/recommendations', artistController.getArtistRecommendations);
 
-  app.get('*', function(req, res) {
-    return res.render('index.html');
+  app.get('*', function(req, res) { 
+    return res.render('index.html'); 
   });
 };

4b0677599432d509020ed080aedf9b94b54f6be0
Dylan Aspden <dhaspden@gmail.com>
2015-01-12 15:29:20 -0500

    Refactor API calls and create like related calls

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,8 +1,33 @@
 var _ = require('lodash');
+var queue = require('../workers/queue');
 
 exports.isLoggedIn = function(req, res) {
-  if (_.isEmpty(req.user)) { return res.status(200).send({ error: 'Not logged in' }); }
-  else {
-    return res.status(200).send({ session: req.session });
+  if (_.isEmpty(req.user)) { 
+    return res.status(200).send(); 
   }
+
+  return res.status(200).send({ session: req.session });
+}
+
+exports.login = function(req, res) {
+  if (_.isEmpty(req.user.values)) { 
+    return res.status(400).send({ error: { message: 'User not found in request.  Please try again.' } }); 
+  }
+
+  queue.crawlUser(req.user.values);
+  res.clearCookie('user')
+  .redirect('/main')
+  .status(200)
+  .send(req.session.cookie);
+}
+
+exports.logout = function(req, res) {
+  if (_.isEmpty(req.session)) { 
+    return res.status(400).send({ error: { message: 'No session currently active to terminate.' } }); 
+  }
+
+  console.log(req.session);
+  delete req.session;
+  console.log(req.session);
+  return res.status(200).send({ success: { message: 'Session destroyed.' } });
 }
--- controllers/genre-controller.js
+++ controllers/genre-controller.js
@@ -5,44 +5,51 @@ var _ = require('lodash');
 /* Returns a list of artists with the given genre (in the query string) */
 exports.getArtistsByGenre = function(req, res) {
   var offset = helper.getOffset(req);
-  if (_.isEmpty(req.query.genre)) { return res.status(400).send('No parameters given') }
-  else {
-    model.Genre.findOne({ where: { genre: req.query.genre } })
-    .then(function(genre) {
-      if (_.isEmpty(genre)) { return res.status(400).send('Genre not found') }
-      else {
-        model.Artist.findAll({ where: { id: { gt: offset }, genre: genre.values.id }, limit: 20 })
-        .then(function(artists) {
-          if (_.isEmpty(artists)) { return res.status(400).send('No artist with genre found') }
-          else {
-            var offset = _.max(artists, function(artist) { return artist.values.id; }).values.id;
-            var nextUrl = 'http://localhost:3000/api/genre/artists?genre=' + genre.values.genre + '&offset=' + offset;
-            return res.status(200).send({ data: artists, paging: { nextUrl: nextUrl } });
-          }
-        })
-        .catch(function(err) {
-          return res.status(400).send(err);
-        });
+  if (_.isEmpty(req.query.genre)) { 
+    return res.status(400).send({ error: { message: 'Input genre query not provided.' } }); 
+  }
+
+  model.Genre.findOne({ where: { genre: req.query.genre } })
+  .then(function(genre) {
+    if (_.isEmpty(genre)) { 
+      return res.status(400).send({ error: { message: 'Input genre query not found in database.' } });
+    }
+    model.Artist.findAll({ where: { id: { gt: offset }, genre: genre.values.id }, limit: 20 })
+    .then(function(artists) {
+      if (_.isEmpty(artists)) { 
+        return res.status(200).send({ data: {}, paging: {} }); 
       }
+      return res.status(200).send({ 
+        data: artists, 
+        paging: { nextUrl: 'http://localhost:3000/api/genre/artists?genre=' + genre.values.genre + 
+                           '&offset=' + helper.calculateOffset(artists) } 
+      });
     })
     .catch(function(err) {
       return res.status(400).send(err);
     });
-  }
+  })
+
+  .catch(function(err) {
+    return res.status(400).send(err);
+  });
 }
 
 /* Gets all of the genres stored in the database */
 exports.getGenres = function(req, res) {
   var offset = helper.getOffset(req);
+
   model.Genre.findAll({ where: { id: { gt: offset } }, limit: 20 })
   .then(function(genres) {
-    if (_.isEmpty(genres)) { return res.status(400).send('No genres found') }
-    else {
-      var offset = _.max(genres, function(genre) { return genre.values.id; }).values.id;
-      var nextUrl = 'http://localhost:3000/api/genre?offset=' + offset;
-      return res.status(200).send({ data: genres , paging: { nextUrl: nextUrl } });
+    if (_.isEmpty(genres)) { 
+      return res.status(400).send({ data: {}, paging: {} }); 
     }
+    return res.status(200).send({ 
+      data: genres, 
+      paging: { nextUrl: 'http://localhost:3000/api/genre?offset=' + helper.calculateOffset(genres) } 
+    });
   })
+
   .catch(function(err) {
     return res.status(400).send(err);
   });

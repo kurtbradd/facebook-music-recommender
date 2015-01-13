21e826269bcb2bc04c06ebfc8e728d73a330d4f6
Brandon Byskov <brandonbyskov@gmail.com>
2015-01-13 10:56:42 -0500

    adjust main page into one more template

---
+++ public/artist_stub.php
@@ -0,0 +1,10 @@
+<div class="artist_section">
+    <div class="artist_thumbnail_container_-1">
+        <div class="artist_thumbnail_container_0">
+            <div class="artist_thumbnail_container_1">
+                <img class="artist_thumbnail" src="https://s3.amazonaws.com/coursera/topics/datasci/small-icon.hover.png"/>
+            </div>
+        </div>
+    </div>
+    <div class="artist_name_heading">Artist Name really long band name</div>
+</div>

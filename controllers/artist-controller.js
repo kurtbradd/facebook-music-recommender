79a13275567c97498348022be133775dfc6b6e34
Kurt DaCosta <kurtbradd@gmail.com>
2015-01-13 03:26:20 -0500

    getArtist route returns recommendations from engine

--- controllers/artist-controller.js
+++ controllers/artist-controller.js
@@ -1,5 +1,6 @@
 var model = require('../models/index');
 var _ = require('lodash');
+var Predictions = require('../helpers/Engine.js');
 
 /* Retrieves an artist with the given Facebook id number */
 exports.getArtist = function(req, res) {
@@ -58,5 +59,30 @@ exports.getGenreOfArtist = function(req, res) {
 
 /* Retrieves the artist recommendations for the user */
 exports.getArtistRecommendations = function(req, res) {
-  return res.status(200).send('Success');
+  if (!req.user) return res.status(400).send('Not Logged In');
+  console.log(req.user);
+  // hard coded for Dylans profileId (v1.0)
+  Predictions.recommendationsForUser(672280485, 40, 
+    function (err, results) {
+      // var artistInfo = {};
+      // var recommendations = results.itemScores;
+      // var done = _.after(users.length, function () {
+      //   _.map(recommendations, function (recommmendation) {
+      //     return {
+      //       "recommmendation": recommendation.score,
+      //       "info": artistInfo[recommmendation.item]
+      //     };
+      //   });
+      // });
+      // _.each(recommendations, function (recommmendation) {
+      //   console.log(recommmendation)
+      //   model.Artist.find({ where: { facebookId: recommmendation.item } })
+      //   .then(function(artist) {
+      //     if (_.isEmpty(artist)) return;
+      //     artistInfo[recommmendation.item] = artist;
+      //     done()
+      //   })
+      // })
+      return res.status(200).send(results);
+  })
 }

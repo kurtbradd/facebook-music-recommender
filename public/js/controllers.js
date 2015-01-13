84259e30600feff5569bd53b8eb9cc263a6a1d66
Kurt DaCosta <kurtbradd@gmail.com>
2015-01-13 03:27:26 -0500

    parse recommendations on client

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,8 +8,11 @@ module.controller('LoginController', ['$scope', 'LoginService',
   }
 ]);
 
-module.controller('AppController', ['$scope', '$state', 'LoginService',
-  function($scope, $state, $cookies, LoginService) {
+module.controller('AppController', ['$scope','$state','$cookies','LoginService','PredictionService','GraphAPI',
+  function($scope, $state, $cookies, LoginService, PredictionService, GraphAPI) {
+
+    $scope.recommendations = null;
+
     $scope.logout = function() {
       LoginService.logout(function(error) {
         if (error === undefined) {
@@ -25,5 +28,32 @@ module.controller('AppController', ['$scope', '$state', 'LoginService',
       facebookUrl += JSON.parse($cookies.user).accessToken;
       return facebookUrl;
     }
+
+    PredictionService.getRecommendations(function (err, results) {
+      var recommendations = results.itemScores;
+      var artistInfo = {};
+      var count = 0;
+      var done = function () {
+        count++;
+        if (count == recommendations.length) {
+          $scope.recommendations = recommendations.map(function (map) {
+            var artistData = artistInfo[map.item];
+            var isArtist = null;
+            if (artistData && artistData.hasOwnProperty('category')) {
+              if (artistData.category == "Musician/band") isArtist = true;
+            }
+            return {artistInfo: artistData, score: map.score, isArtist:isArtist};
+          }).filter(function (filter) {
+            if (filter.isArtist) return true;
+          });
+        }
+      }
+      recommendations.forEach(function (result) {
+        GraphAPI.getInfoForPage(result.item, function (err, data) {
+          done();
+          if (data && data.name) artistInfo[result.item] = data;
+        })
+      })
+    });
   }
 ]);
